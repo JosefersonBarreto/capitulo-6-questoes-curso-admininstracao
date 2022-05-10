d9b9837aa2bb97c6039cbf844af8268af241b338
Joseferson Barreto <joseferson7@gmail.com>
2022-05-10 18:14:18 -0300

    up de arquivos

---
+++ pinguins.R
@@ -0,0 +1,18 @@
+install.packages("palmerpenguins")
+
+
+
+#==============================================================================
+#                script atividade   topicos -prof.Thiago
+#==============================================================================
+
+
+
+
+library(palmerpenguins)
+
+palmerpenguins::penguins
+
+
+dados=penguins
+   
